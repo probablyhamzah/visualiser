98487859ef1af94237b0bb009255c3bb0b8f223b
Ahmed Hamzah <ahmedhamzah2002@gmail.com>
2020-11-23 23:36:26 +0530

    readme isn't appearing on the pypi site

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
     name = 'visualiser',   
     packages = ['visualiser'],
-    version = '0.5',
+    version = '0.6',
     license='MIT',
     description = 'A terminal visualiser.',
     author = 'possiblyhamzah',
     author_email = 'possiblyhamzah@gmail.com',
     url = 'https://github.com/probablyhamzah/visualiser',
-    download_url = 'https://github.com/probablyhamzah/visualiser/archive/v_05.tar.gz',
+    download_url = 'https://github.com/probablyhamzah/visualiser/archive/v_06.tar.gz',
     keywords = ['visualiser', 'terminal'],
     install_requires=[
             'librosa',
