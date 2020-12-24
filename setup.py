c3dfb59e31fc3d37258fd073d9ec9a15bb17bafc
possiblyhamzah <possiblyhamzah@gmail.com>
2020-12-24 15:42:21 +0530

    editing setup

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
-from distutils.core import setup
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 setup(
     name = 'visualiser',   
     packages = ['visualiser'],
     version = '0.8',
     license='MIT',
     description = 'A terminal visualiser.',
+    long_description=README,
+    long_description_content_type='text/markdown',
     author = 'possiblyhamzah',
     author_email = 'possiblyhamzah@gmail.com',
     url = 'https://github.com/probablyhamzah/visualiser',
